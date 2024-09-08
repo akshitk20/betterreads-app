6b68277e6e5359b3b6e130020027426a32a5fb7b
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-09-08 20:35:31 +0530

    completed basic oauth2 login using github

--- src/main/java/com/springboot/betterreads_app/BetterreadsAppApplication.java
+++ src/main/java/com/springboot/betterreads_app/BetterreadsAppApplication.java
@@ -2,12 +2,21 @@ package com.springboot.betterreads_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BetterreadsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsAppApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public String getUser(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getName();
 	}
 
 }
---
+++ src/main/java/com/springboot/betterreads_app/SecurityAdaptor.java
@@ -0,0 +1,34 @@
+package com.springboot.betterreads_app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityAdaptor  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .oauth2Login();
+        return http.build();
+    }
+
+}
