0f980e6318b7369218a912c254663bcb313df8e9
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-09-08 20:17:13 +0530

    initial commit

---
+++ src/test/java/com/springboot/betterreads_app/BetterreadsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.betterreads_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
