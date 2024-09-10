43b7dc326f03038ea4253a216cebcdee2c8e3929
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-09-10 20:38:53 +0530

    completed BetterReads Oauth

--- src/main/java/com/springboot/betterreads_app/BetterreadsAppApplication.java
+++ src/main/java/com/springboot/betterreads_app/BetterreadsAppApplication.java
@@ -5,18 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BetterreadsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsAppApplication.class, args);
 	}
 
-	@RequestMapping("/user")
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public String getUser(@AuthenticationPrincipal OAuth2User principal) {
 		System.out.println(principal);
-		return principal.getName();
+		return principal.getAttribute("login");
 	}
 
 }
