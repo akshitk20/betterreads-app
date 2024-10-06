871148b3a2bfed517146e95918e26f03de936cf0
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-10-06 13:06:15 +0530

    completed book.html form

--- src/main/java/com/springboot/betterreads_app/controller/UserBooksController.java
+++ src/main/java/com/springboot/betterreads_app/controller/UserBooksController.java
@@ -1,15 +1,25 @@
 package com.springboot.betterreads_app.controller;
 
+import com.springboot.betterreads_app.repository.userbooks.UserBooksRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@RequiredArgsConstructor
 public class UserBooksController {
 
-//    @PostMapping("/addUserBook")
-//    public String addBookForUser(@AuthenticationPrincipal OAuth2User principal) {
-//
-//    }
+    private final UserBooksRepository userBooksRepository;
+
+    @PostMapping("/addUserBook")
+    public String addBookForUser(@RequestBody MultiValueMap<String, String> formData,
+                                 @AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(formData);
+        System.out.println(principal);
+        return "";
+    }
 }
