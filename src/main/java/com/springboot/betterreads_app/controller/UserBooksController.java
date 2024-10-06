09fb05cf7cb4dd6cd694ef62dcc7be8650b1d0c1
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-10-06 13:22:01 +0530

    completed userbooks

--- src/main/java/com/springboot/betterreads_app/controller/UserBooksController.java
+++ src/main/java/com/springboot/betterreads_app/controller/UserBooksController.java
@@ -1,5 +1,7 @@
 package com.springboot.betterreads_app.controller;
 
+import com.springboot.betterreads_app.model.userbooks.UserBooks;
+import com.springboot.betterreads_app.model.userbooks.UserBooksPrimaryKey;
 import com.springboot.betterreads_app.repository.userbooks.UserBooksRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,6 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,10 +22,24 @@ public class UserBooksController {
     private final UserBooksRepository userBooksRepository;
 
     @PostMapping("/addUserBook")
-    public String addBookForUser(@RequestBody MultiValueMap<String, String> formData,
-                                 @AuthenticationPrincipal OAuth2User principal) {
+    public ModelAndView addBookForUser(@RequestBody MultiValueMap<String, String> formData,
+                                       @AuthenticationPrincipal OAuth2User principal) {
+        if (principal == null || principal.getAttribute("login") == null) {
+            return null;
+        }
         System.out.println(formData);
         System.out.println(principal);
-        return "";
+        UserBooks userBooks = new UserBooks();
+        UserBooksPrimaryKey key = new UserBooksPrimaryKey();
+        key.setUserId(principal.getAttribute("login"));
+        String bookId = formData.getFirst("bookId");
+        key.setBookId(bookId);
+        userBooks.setStartedDate(LocalDate.parse(Objects.requireNonNull(formData.getFirst("startDate"))));
+        userBooks.setCompletedDate(LocalDate.parse(Objects.requireNonNull(formData.getFirst("completedDate"))));
+        userBooks.setRating(Integer.parseInt(Objects.requireNonNull(formData.getFirst("rating"))));
+        userBooks.setReadingStatus(formData.getFirst("status"));
+
+        userBooksRepository.save(userBooks);
+        return new ModelAndView("redirect:/books/" + bookId);
     }
 }
--- src/main/java/com/springboot/betterreads_app/model/userbooks/UserBooks.java
+++ src/main/java/com/springboot/betterreads_app/model/userbooks/UserBooks.java
@@ -2,9 +2,10 @@ package com.springboot.betterreads_app.model.userbooks;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.mapping.*;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.time.LocalDate;
 
